25c2ff7292428ec41ed48d214f367935fc739b06
Alvin Biju <bijualvin2@gmail.com>
2026-04-12 14:26:24 -0400

    Update unitTests.ts

--- frontend/src/__tests__/unitTests.ts
+++ frontend/src/__tests__/unitTests.ts
@@ -84,8 +84,8 @@ describe('formatDateReservation', () => {
 
 describe('isPhone', () => {
     it('return true when valid value is inputted', () => {
-        const result = isPhone("2024-06-15T14:30:00.000Z");
-        expect(result).toBe(false);
+         const result = isPhone("5145145144");
+        expect(result).toBe(true);
     });
     it('return false when wrong value is inputted', () => {
         const result = isPhone("2024-06-15T14:30:00.000Z");
